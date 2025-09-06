1b13d700b266ac207ecbefe180f42efa094e3df0
Vinay-vaja <vinayvaja2276@gmail.com>
2025-09-06 08:54:59 +0530

    Add files via upload
    src file for baisic calulator

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,23 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import FoodItem from "./components/foodItem";
-import ErrorMsg from "./components/errorMessage";
-import Container from "./components/Container";
-import LanguageInput from "./components/langInput";
-import { useState } from "react";
+
+import styles from "./App.module.css";
+import ButtonContainer from "./Components/ButtonContainer";
+import Display from "./Components/Display";
+
 
 function App() {
-  let[programmingLang,setLanguage]=useState([]);
-  // let programmingLang = ["C", "CPP", "Javascript", "Python", "GO"];
-  // let programmingLang=[];
 
-  // let textState=useState("Hare krishna"); //it is a one type of HOOK also return array in which have 2 types of array
-  // let textToShow=textState[0];
-  // // let textStatemethod=textState[1];
-  // let setTextState=textState[1];
-
-
-  let[textToShow,setTextState]=useState("Python is best");
-  console.log(`the current val is :${textToShow}`);
-
- 
-  const onKeyDown=(event)=>{
-    if(event.key==='Enter'){
-      let newLang=event.target.value;  
-      event.target.value="";
-      let newLanguages=[...programmingLang,newLang]; //...is s spread operator
-      setLanguage(newLanguages);
-    
-    }
-   
-  }
- 
 
   return (
     <>
-    <Container>
-      <h1 className="vk-h1">Programming Languages</h1>
-      <LanguageInput handleKeyDown={onKeyDown}></LanguageInput>
-     
-      <ErrorMsg items={programmingLang}></ErrorMsg>
-      <FoodItem items={programmingLang}></FoodItem>
-     
-    </Container>
-    <Container>
-      <p>Above is the list of Best prgoramming Languages you must have to learn these.</p>
-    </Container>
+     <div className={styles.calculator}> 
+      {/* //use class name using styles. ok */}
+      <Display></Display>
+      <ButtonContainer></ButtonContainer>
+      
+       
+     </div>
     </>
-  );
+  )
 }
 
-export default App;
-
-//falsy values are : null,undefined,zero,NaN,empty ->JSX ignore this
+export default App
